f599808f5c69a0edc0b0be7119edd9ddea14408c
PauloHGrande <pauloav2013@gmail.com>
2024-12-10 14:09:42 -0300

    endPoint de findById para Tecnico

---
+++ src/main/java/com/fantasma/helpdesk/services/TecnicoService.java
@@ -0,0 +1,21 @@
+package com.fantasma.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fantasma.helpdesk.domain.Tecnico;
+import com.fantasma.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class TecnicoService {
+
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	
+	public Tecnico findById(Integer id) {
+		Optional<Tecnico> obj = tecnicoRepository.findById(id);
+		return obj.orElse(null);
+	}
+}
