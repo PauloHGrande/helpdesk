2206282afc9323a85a657a872c0d1031eea9fceb
PauloHGrande <pauloav2013@gmail.com>
2024-12-10 15:42:20 -0300

    EndPoint findAll para tecnicos

--- src/main/java/com/fantasma/helpdesk/services/TecnicoService.java
+++ src/main/java/com/fantasma/helpdesk/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.fantasma.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,9 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = tecnicoRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Tecnico não Encontrado! id: " + id));
+	}
+
+	public List<Tecnico> findAll() {
+		return tecnicoRepository.findAll();
 	}
 }
