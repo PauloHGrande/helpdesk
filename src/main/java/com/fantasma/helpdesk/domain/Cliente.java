4d878fc47b9971b4904d4651aa0907e4d89a7a83
PauloHGrande <pauloav2013@gmail.com>
2024-12-11 16:44:00 -0300

    CRUD para Cliente implementado

--- src/main/java/com/fantasma/helpdesk/domain/Cliente.java
+++ src/main/java/com/fantasma/helpdesk/domain/Cliente.java
@@ -2,7 +2,9 @@ package com.fantasma.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.fantasma.helpdesk.domain.dtos.ClienteDTO;
 import com.fantasma.helpdesk.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -36,5 +38,14 @@ public class Cliente extends Pessoa {
 		this.chamados = chamados;
 	}
 	
-	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+	}
 }
---
+++ src/main/java/com/fantasma/helpdesk/services/ClienteService.java
@@ -0,0 +1,71 @@
+package com.fantasma.helpdesk.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fantasma.helpdesk.domain.Pessoa;
+import com.fantasma.helpdesk.domain.Cliente;
+import com.fantasma.helpdesk.domain.dtos.ClienteDTO;
+import com.fantasma.helpdesk.repositories.PessoaRepository;
+import com.fantasma.helpdesk.repositories.ClienteRepository;
+import com.fantasma.helpdesk.services.exceptions.DataIntegrityViolationException;
+import com.fantasma.helpdesk.services.exceptions.ObjectNotFoundException;
+
+import jakarta.validation.Valid;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Cliente findById(Integer id) {
+		Optional<Cliente> obj = clienteRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Cliente não Encontrado! id: " + id));
+	}
+
+	public List<Cliente> findAll() {
+		return clienteRepository.findAll();
+	}
+
+	public Cliente create(ClienteDTO objDTO) {
+		objDTO.setId(null);
+		validaPorCpfeEmail(objDTO);
+		Cliente newObj = new Cliente(objDTO);
+		return clienteRepository.save(newObj);
+	}
+
+	private void validaPorCpfeEmail(ClienteDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+		}
+		
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-Mail já cadastrado no sistema!");
+		}
+	}
+
+	public Cliente update(Integer id, @Valid ClienteDTO objDTO) {
+		objDTO.setId(id);
+		Cliente oldObj = findById(id);
+		validaPorCpfeEmail(objDTO);
+		oldObj = new Cliente(objDTO);
+		return clienteRepository.save(oldObj);
+	}
+
+	public void delete(Integer id) {
+		Cliente obj = findById(id);
+		if(obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Cliente possui ordens de serviço e não pode ser deletado!");
+		}
+		clienteRepository.deleteById(id);
+	}
+}
