dc28051592b8bdcfbaa030e6f89caf54d92621c6
PauloHGrande <pauloav2013@gmail.com>
2024-12-11 17:11:44 -0300

    FindById para Chamado

---
+++ src/main/java/com/fantasma/helpdesk/services/ChamadoService.java
@@ -0,0 +1,23 @@
+package com.fantasma.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fantasma.helpdesk.domain.Chamado;
+import com.fantasma.helpdesk.repositories.ChamadoRepository;
+import com.fantasma.helpdesk.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ChamadoService {
+
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+	
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = chamadoRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Chamado não Encontrado! id: " + id));
+	
+	}
+}
