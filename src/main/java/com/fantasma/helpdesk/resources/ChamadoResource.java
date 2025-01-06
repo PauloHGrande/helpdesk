11b7fc8a52535433f7b188f298b5390d286e1aa5
PauloHGrande <pauloav2013@gmail.com>
2025-01-06 14:31:23 -0300

    Ajustando a Class de Chamados

--- src/main/java/com/fantasma/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/fantasma/helpdesk/resources/ChamadoResource.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fantasma.helpdesk.domain.Chamado;
@@ -20,6 +21,7 @@ import com.fantasma.helpdesk.services.ChamadoService;
 
 import jakarta.validation.Valid;
 
+@RestController
 @RequestMapping(value = "/chamados")
 public class ChamadoResource {
 
